798e4684eeaf88e765897b86f6b3184da214f62f
xiongben <wangdongkf@163.com>
2018-03-20 20:38:09 +0800

    修复注册到cloud云

--- src/main/java/com/gws/controllers/oss/OssController.java
+++ src/main/java/com/gws/controllers/oss/OssController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
